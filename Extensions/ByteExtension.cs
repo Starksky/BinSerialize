10564eebb139708fe24a323be4af0bddc9294158
Stanislav <sufiks777@gmail.com>
2021-11-13 15:24:01 +0300

    push

---
+++ Extensions/ByteExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Serialization.Extensions
+{
+    public static class ByteExtension
+    {
+        public static byte[] GetBytes(this byte value)
+        {
+            return new[]{ value };
+        }
+        public static byte GetValue(byte[] data)
+        {
+            return data[0];
+        }
+        public static byte GetValue(byte[] data, ref int offset)
+        {
+            return data[offset++];
+        }
+    }
+}
