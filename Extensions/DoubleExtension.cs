1c8b8f9adfc852a980908ac154516c47a7d0a4a3
Stanislav <stas.loktev2010@yandex.ru>
2022-01-21 13:35:53 +0300

    fix

---
+++ Extensions/DoubleExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Serialization.Extensions
+{
+    public static class DoubleExtension
+    {
+        public static byte[] GetBytes(this double value)
+        {
+            return BitConverter.GetBytes(value);
+        }
+        public static double GetValue(byte[] data)
+        {
+            return BitConverter.ToDouble(data, 0);
+        }
+        public static double GetValue(byte[] data, ref int offset)
+        {
+            double result = BitConverter.ToDouble(data, offset);
+            offset += sizeof(double);
+            return result;
+        }
+    }
+}
